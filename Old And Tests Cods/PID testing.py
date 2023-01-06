f3383f8bc1005aaa43771548e10165999f8d0fe8
cooligrek <cooligrek@gmail.com>
2023-01-05 18:31:07 -0800

    updated code

--- Old And Tests Cods/PID testing.py
+++ Old And Tests Cods/PID testing.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import math
 
@@ -33,3 +34,40 @@ plt.plot(FlowRateArr)
 
 #plt.ylabel('flowrate')
 plt.show()
+=======
+import matplotlib.pyplot as plt
+import math
+
+PressureNeeded = 50
+PressureCurr = 10
+PressureCurr1 = 10
+sinePressure = 10
+
+Tolerance = 1
+ProportionalTerm = 0.005
+
+PressureArr = []
+FlowRateArr = []
+Error = PressureNeeded - PressureCurr
+ErrorStart = PressureNeeded - PressureCurr
+Error1 = PressureNeeded - PressureCurr1
+
+holeArea = 3.1415*(1/8)**2/4
+
+
+while abs(Error) > Tolerance:
+    PressureArr.append(PressureCurr)
+    FlowRateArr.append(PressureCurr1)
+    PercChange = (PressureNeeded-PressureCurr)*0.005
+    PressureCurr += Error*(math.sin(Error/ErrorStart*math.pi)+1)*ProportionalTerm
+    PressureCurr1 += Error1*PercChange
+    Error = PressureNeeded - PressureCurr
+    Error1 = PressureNeeded - PressureCurr1
+
+    
+plt.plot(PressureArr)
+plt.plot(FlowRateArr)
+
+#plt.ylabel('flowrate')
+plt.show()
+>>>>>>> 0639761 (updated code)
