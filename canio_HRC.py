f97819cdd16d14c5fe6af66c954433497e5fa11d
Joe Kessler <copperbotte@gmail.com>
2024-04-02 04:05:28 -0700

    Completes CanIO backend
    This commit does not integrate HRC messaging to the front end.

--- canio_HRC.py
+++ canio_HRC.py
@@ -280,7 +280,7 @@ def intTypeCheck(var, type, label, size):
 
 class CanSend:
     def __init__(self, **busargs):
-        print("RocketDriver2 CanSend instance created")
+        print("Hericlitus Rocket Controller CanSend instance created")
         self.bus = can.interface.Bus(**busargs)
 
     def send(self, ID, DATA):
@@ -615,284 +615,133 @@ class CanSend:
 ################################################################################
 
 class CanReceive:
-    VehicleStates = [
-        "Setup",
-        "Passive",
-        "Standby",
-        "Test",
-        "Abort",
-        "Vent",
-        "OFF Nominal",
-        "Hi Press Arm",
-        "Hi Press Pressurized",
-        "Tank Press Arm",
-        "Tank Press Pressurized",
-        "Fire Arm",
-        "Fire"
-        ]
     
-    def __init__(self, channel='can0', bustype='socketcan'):
-        print("RocketDriver2 CanReceive instance created")
-        self.loop = True
-        self.busargs = {'channel':channel, 'bustype':bustype}
+    StateLUT = {
+        IGN1_ID : [ IGN1_OFF,  IGN1_ON],
+        IGN2_ID : [ IGN2_OFF,  IGN2_ON],
+        HP_ID   : [ HP_CLOSE,  HP_OPEN],
+        HV_ID   : [ HV_CLOSE,  HV_OPEN],
+        FMV_ID  : [FMV_CLOSE, FMV_OPEN],
+        LMV_ID  : [LMV_CLOSE, LMV_OPEN],
+        LV_ID   : [ LV_CLOSE,  LV_OPEN],
+        LDV_ID  : [LDV_CLOSE, LDV_OPEN],
+        LDR_ID  : [LDR_CLOSE, LDR_OPEN],
+        FV_ID   : [ FV_CLOSE,  FV_OPEN],
+        FDV_ID  : [FDV_CLOSE, FDV_OPEN],
+        FDR_ID  : [FDR_CLOSE, FDR_OPEN],
+    }
 
-        self.Sensors = [0] * 1028
-        self.sensorTimestamps = [0] * 1028
-        self.Valves = [0] * 64
-        self.ValvesRenegadeEngine = [0] * 64
-        self.ValvesRenegadeProp = [0] * 64
-        self.Controllers = [[0] * 50 for i in range(12)]
+    StateKeys = {
+        SR_ENGINE:[HP_ID,  HV_ID, FMV_ID, LMV_ID, IGN1_ID, IGN2_ID],
+        SR_PROP:  [LV_ID, LDV_ID, LDR_ID,  FV_ID,  FDV_ID,  FDR_ID]
+    }
 
-        self.NodeStatusBang = CanReceive.VehicleStates[0]
-        self.NodeStatusRenegadeEngine = CanReceive.VehicleStates[0]
-        self.NodeStatusRenegadeProp = CanReceive.VehicleStates[0]
+    SensorLUT = {
+        PT_LOX_HIGH_ID      : dict(pin=PT_LOX_HIGH_PIN,      m=PT_LOX_HIGH_CAL_M,      b=PT_LOX_HIGH_CAL_B),
+        PT_FUEL_HIGH_ID     : dict(pin=PT_FUEL_HIGH_PIN,     m=PT_FUEL_HIGH_CAL_M,     b=PT_FUEL_HIGH_CAL_B),
+        PT_LOX_DOME_ID      : dict(pin=PT_LOX_DOME_PIN,      m=PT_LOX_DOME_CAL_M,      b=PT_LOX_DOME_CAL_B),
+        PT_FUEL_DOME_ID     : dict(pin=PT_FUEL_DOME_PIN,     m=PT_FUEL_DOME_CAL_M,     b=PT_FUEL_DOME_CAL_B),
+        PT_LOX_TANK_1_ID    : dict(pin=PT_LOX_TANK_1_PIN,    m=PT_LOX_TANK_1_CAL_M,    b=PT_LOX_TANK_1_CAL_B),
+        PT_LOX_TANK_2_ID    : dict(pin=PT_LOX_TANK_2_PIN,    m=PT_LOX_TANK_2_CAL_M,    b=PT_LOX_TANK_2_CAL_B),
+        PT_FUEL_TANK_1_ID   : dict(pin=PT_FUEL_TANK_1_PIN,   m=PT_FUEL_TANK_1_CAL_M,   b=PT_FUEL_TANK_1_CAL_B),
+        PT_FUEL_TANK_2_ID   : dict(pin=PT_FUEL_TANK_2_PIN,   m=PT_FUEL_TANK_2_CAL_M,   b=PT_FUEL_TANK_2_CAL_B),
+        PT_PNUEMATICS_ID    : dict(pin=PT_PNUEMATICS_PIN,    m=PT_PNUEMATICS_CAL_M,    b=PT_PNUEMATICS_CAL_B),
+        PT_LOX_INLET_ID     : dict(pin=PT_LOX_INLET_PIN,     m=PT_LOX_INLET_CAL_M,     b=PT_LOX_INLET_CAL_B),
+        PT_FUEL_INLET_ID    : dict(pin=PT_FUEL_INLET_PIN,    m=PT_FUEL_INLET_CAL_M,    b=PT_FUEL_INLET_CAL_B),
+        PT_FUEL_INJECTOR_ID : dict(pin=PT_FUEL_INJECTOR_PIN, m=PT_FUEL_INJECTOR_CAL_M, b=PT_FUEL_INJECTOR_CAL_B),
+        PT_CHAMBER_1_ID     : dict(pin=PT_CHAMBER_1_PIN,     m=PT_CHAMBER_1_CAL_M,     b=PT_CHAMBER_1_CAL_B),
+        PT_CHAMBER_2_ID     : dict(pin=PT_CHAMBER_2_PIN,     m=PT_CHAMBER_2_CAL_M,     b=PT_CHAMBER_2_CAL_B),
+                         -1 : dict(pin=-1,                   m=1,                      b=0), # Padding
+    }
 
-        self.AutosequenceTime = 0
-        self.ThrottlePoints = {}
-        self.AutosequenceTimeDupes = 0
+    SensorKeys = {
+        SENS_1_4_PROP:     [PT_LOX_HIGH_ID,   PT_FUEL_HIGH_ID,  PT_LOX_DOME_ID,    PT_FUEL_DOME_ID],
+        SENS_5_8_PROP:     [PT_LOX_TANK_1_ID, PT_LOX_TANK_2_ID, PT_FUEL_TANK_1_ID, PT_FUEL_TANK_2_ID],
+        SENS_9_12_ENGINE:  [PT_PNUEMATICS_ID, PT_LOX_INLET_ID,  PT_FUEL_INLET_ID,  PT_FUEL_INJECTOR_ID],
+        SENS_13_16_ENGINE: [PT_CHAMBER_1_ID,  PT_CHAMBER_2_ID,  -1,                -1]
+    }
 
+    def __init__(self, **busargs):
+        print("Hericlitus Rocket Controller CanReceive instance created")
+        self.bus = can.interface.Bus(**busargs)
+
+        self.States = {k:v[0] for k,v in self.StateLUT.items()}
+        self.Sensor_Raw = {k:0 for k,v in self.SensorLUT.items()}
+        self.Sensor_Val = {k:v['b'] for k,v in self.SensorLUT.items()}
+
+    @docstring("""
+    Runs the CanReceive thread.
+    """)
     def run(self):
-        # starts Canbus
-        #bus_type = 'virtual'#'socketcan'
-        #channel0 = 'vcan0'
-        bus_receive = can.interface.Bus(**self.busargs)#channel=channel0, bustype=bus_type)
-        ###print("initializing new can")
-        ###print()
-        ###yield # initial yield to init the bus. Why isn't this in init??
-        
-        while self.loop:
-            ###print("waiting for message in...")
-            msg_in = bus_receive.recv(timeout=None)
-            ###print("bus mentioned")
+        self.loop = True
 
+        for cycle in self.generator():
+            self.loop = cycle
+
+    @docstring("""
+    Returns a generator that runs a single message loop cycle.
+    """)
+    def generator(self):
+        while self.loop:
+            msg_in = self.bus.recv(timeout=None)
             try:
-                ID_A, msg_id_bin, data_bin, data_list_hex = \
-                      self.parseMessage(msg_in)
+                yield self.translateMessage(*self.parseMessage(msg_in))
             except Exception as e:
                 print(e)
-                continue
-        
-            ###print("parsed message as:")
-            ###print(ID_A, data_list_hex)
-            ###print("")
-            ###print("translating message:")
-            self.translateMessage(ID_A, msg_id_bin, data_bin, data_list_hex)
-            ###print("")
-            ###print("yield")
-            ###yield
+            
 
+    @docstring("""
+    # Parses a message recieved by the Can Bus.
+    # Returns message id as an integer, and the data in hex and binary formats.
+    # 
+    # Arguments:
+    # msg_in: Can message
+    """)
     def parseMessage(self, msg_in):
         # Grabs Message ID
         msg_id = int(msg_in.arbitration_id)
-        msg_id_bin = str(bitstring.BitArray(int=msg_id, length=32).bin)
-        ID_A_bin = msg_id_bin[-11:]
-        ID_A = ba2int(bitarray(ID_A_bin))
+        
         # Grabs the data in the msg
         data_list_hex = msg_in.data.hex()
         data_bin = bitstring.BitArray(hex=data_list_hex).bin
-
-        # if data is empty
-#       if data_list_hex[0:16] == '':
-#           continue
         
-        return ID_A, msg_id_bin, data_bin, data_list_hex
+        return msg_id, data_list_hex, data_bin
     
-    def translateMessage(self, ID_A, msg_id_bin, data_bin, data_list_hex):
+    @docstring("""
+    # Passes the parsed message to the appropriate decoder.
+    """)
+    def translateMessage(self, msg_id, data_list_hex, data_bin):
 
-        if ID_A == 546:
-            self.ID_546(ID_A, msg_id_bin, data_bin, data_list_hex)
-        if ID_A == 552:
-            self.ID_552(ID_A, msg_id_bin, data_bin, data_list_hex)
-        if ID_A == 547:
-            self.ID_547(ID_A, msg_id_bin, data_bin, data_list_hex)
-        elif 510 < ID_A < 530:
-            self.ID_Between_510_530(ID_A, msg_id_bin, data_bin, data_list_hex)
-        elif 50 < ID_A < 427:
-            self.ID_Between_050_427(ID_A, msg_id_bin, data_bin, data_list_hex)
-            
-
-        elif ID_A > 1000:
-            self.translateControllerMessage(ID_A, msg_id_bin, data_bin, data_list_hex)
-
-    def translateControllerMessage(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        " CONTROLLERS"
-        #print(ID_A)
-
-        if ID_A == 1100:
-            self.ID_1100_Controller(ID_A, msg_id_bin, data_bin, data_list_hex)
-        elif ID_A == 1506:
-            self.ID_1506_Controller(ID_A, msg_id_bin, data_bin, data_list_hex)
-        elif data_list_hex:
-            self.ID_Misc_Controller(ID_A, msg_id_bin, data_bin, data_list_hex)
-
-    def ID_546(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        """
-        Valves
+        if msg_id in self.StateKeys:
+            return self.recvStateReport(msg_id, data_list_hex, data_bin)
         
-        """
-        #print(msg_id_bin, data_bin)
-        HP1_bin = int(msg_id_bin[12:20])
-        HP1_bin = str(HP1_bin)[::-1]
-        if HP1_bin:
-            HP1 = ba2int(bitarray(HP1_bin))
-            #print(HP1)
-            #print(msg_id_bin)
-            self.ValvesRenegadeEngine[1] = HP1
-
-        HP2_bin = int(msg_id_bin[4:12])
-        HP2_bin = str(HP2_bin)[::-1]
-        HP2 = ba2int(bitarray(HP2_bin))
-
-        self.ValvesRenegadeEngine[2] = HP2
-        for i in range(3, 11):
-            HPi_bin = data_bin[(i - 3) * 8:(i - 2) * 8]
-            HPi = ba2int(bitarray(HPi_bin))
-            self.ValvesRenegadeEngine[i] = HPi
-    
-    def ID_552(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        """
-        Valves
+        if msg_id in self.SensorKeys:
+            return self.recvSensorData(msg_id, data_list_hex, data_bin)
         
-        """
-        #print(msg_id_bin, data_bin)
-        HP1_bin = int(msg_id_bin[12:20])
-        HP1_bin = str(HP1_bin)[::-1]
-        if HP1_bin:
-            HP1 = ba2int(bitarray(HP1_bin))
-            #print(HP1)
-            #print(msg_id_bin)
-            self.Valves[1] = HP1
+        raise KeyError("Unknown message recieved with id: %d, and data [%s]"%(msg_id, str(data_list_hex)))
 
-        HP2_bin = int(msg_id_bin[4:12])
-        HP2_bin = str(HP2_bin)[::-1]
-        HP2 = ba2int(bitarray(HP2_bin))
+    @docstring("""
+    # State report
+    """)
+    def recvStateReport(self, msg_id, data_list_hex, data_bin):
 
-        self.Valves[2] = HP2
-        for i in range(3, 11):
-            HPi_bin = data_bin[(i - 3) * 8:(i - 2) * 8]
-            HPi = ba2int(bitarray(HPi_bin))
-            self.Valves[i] = HPi
+        self.time_micros = int('0'+data_bin[:32], base=2)
+        self.rocketState = int('0'+data_list_hex[4], base=16)
 
-    def ID_547(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        """
-        Valves
-        
-        """
-        #print(msg_id_bin, data_bin)
-        HP1_bin = int(msg_id_bin[12:20])
-        HP1_bin = str(HP1_bin)[::-1]
-        if HP1_bin:
-            HP1 = ba2int(bitarray(HP1_bin))
-            #print(HP1)
-            #print(msg_id_bin)
-            self.ValvesRenegadeProp[1] = HP1
+        state_bits = '{0:06b}'.format(int('0'+data_list_hex[5], base=16))
+        states = list(map(lambda b: int('0'+b, base=2), state_bits))
 
-        HP2_bin = int(msg_id_bin[4:12])
-        HP2_bin = str(HP2_bin)[::-1]
-        HP2 = ba2int(bitarray(HP2_bin))
+        for key, state in zip(self.StateKeys[msg_id], states):
+            self.States[key] = self.StateLUT[key][state]
 
-        self.ValvesRenegadeProp[2] = HP2
-        for i in range(3, 11):
-            HPi_bin = data_bin[(i - 3) * 8:(i - 2) * 8]
-            HPi = ba2int(bitarray(HPi_bin))
-            self.ValvesRenegadeProp[i] = HPi
-    
-    def ID_Between_510_530(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        "NODE STATES"
-        "Engine Node 2"
-        "Prop Node 3"
-        try:
-            if ID_A == 514: 
-                self.NodeStatusRenegadeEngine = CanReceive.VehicleStates[int(data_list_hex[0:2], 16)]
-            if ID_A == 515: 
-                self.NodeStatusRenegadeProp = CanReceive.VehicleStates[int(data_list_hex[0:2], 16)]
-            if ID_A == 520: 
-                self.NodeStatusBang = CanReceive.VehicleStates[int(data_list_hex[0:2], 16)]
-            self.translateMessage(ID_A, msg_id_bin, data_bin, data_list_hex)
-        except:
-            return
-        
-    def ID_Between_050_427(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        """
-        Sensors
-        """
-        #print(ID_A, "NADA")
-        if data_list_hex:
-            TimeStamp_bin = msg_id_bin[0:18]
-            TimeStamp = ba2int(bitarray(TimeStamp_bin))
-            msg_id = ID_A
-            value = ba2int(bitarray(data_bin[0:16]), signed = False)
-            self.Sensors[msg_id] = value/10
-            self.sensorTimestamps[msg_id] = TimeStamp
-            #print(msg_id, value/10, data_list_hex,data_bin[0:16])
-            if len(data_list_hex) > 6:
-                msg_id_2 = int(data_list_hex[4:6], base=16)
-                value_2 =  ba2int(bitarray(data_bin[24:40]), signed = False)
-                self.Sensors[msg_id_2] = value_2/10
-                self.sensorTimestamps[msg_id_2] = TimeStamp
-                #print(msg_id_2, value_2/10, data_list_hex,data_bin[24:40])
+    @docstring("""
+    # Sensor data
+    """)
+    def recvSensorData(self, msg_id, data_list_hex, data_bin):
 
-            if len(data_list_hex) > 12:
+        vals = [int('0'+data_bin[i:i+16], base=2) for i in range(0, 64, 16)]
 
-                msg_id_3 = int(data_list_hex[10:12], base=16)
-                value_3 = ba2int(bitarray(data_bin[48:64]), signed = False)
-                self.Sensors[msg_id_3] = value_3/10
-                self.sensorTimestamps[msg_id_3] = TimeStamp
-                #print(msg_id_3, value_3/10, data_list_hex,data_bin[48:64])
-
-    def ID_1100_Controller(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        "AUTOSEQUENCE"
-        self.PrevAutosequenceTime = self.AutosequenceTime
-        self.AutosequenceTime = ba2int(bitarray(data_bin), signed = True)/1000000
-
-        if self.PrevAutosequenceTime == self.AutosequenceTime:
-            self.AutosequenceTimeDupes += 1
-        else:
-            print(self.AutosequenceTimeDupes, "Dupes parsed.")
-            print("Autosequence Time:", self.AutosequenceTime)
-            self.AutosequenceTimeDupes = 0
-
-    def ID_1506_Controller(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        Time = int(data_list_hex[0:4], base=16)
-        ThrottlePoint = int(data_list_hex[4:8], base=16)
-        if Time == 0:
-            self.ThrottlePoints = []
-        #print(Time, ThrottlePoint)
-        self.ThrottlePoints.append([Time, ThrottlePoint])
-        try:
-            Time = int(data_list_hex[8:12], base=16)
-            ThrottlePoint = int(data_list_hex[12:16], base=16)
-            self.ThrottlePoints.append([Time, ThrottlePoint])
-        except:
-            return
-
-    def ID_Misc_Controller(self, ID_A, msg_id_bin, data_bin, data_list_hex):
-        ControllerID = (round(ID_A, -2)-1000)//100
-        ControllerIndex = ID_A % 100
-        if len(data_list_hex) == 16:
-            
-            if ID_A == 1502 or ID_A == 1504:
-                value_1 = ba2int(bitarray(data_bin[0:32]), signed = True)
-                self.Controllers[ControllerID][ControllerIndex] = value_1
-                value_2 = ba2int(bitarray(data_bin[32:64]), signed = True)
-                self.Controllers[ControllerID][ControllerIndex + 1] = value_2
-            elif ControllerIndex == 14 or ControllerIndex == 15:
-                value_1 = int(data_bin[0:32],2)
-                self.Controllers[ControllerID][ControllerIndex] = value_1
-                value_2 = int(data_bin[32:64],2)
-                self.Controllers[ControllerID][ControllerIndex + 1] = value_2
-            else:
-                value_1 = int(data_bin[0:32],2)
-                value_1 = struct.unpack('f', struct.pack('I', value_1))[0]
-                self.Controllers[ControllerID][ControllerIndex] = value_1
-                value_2 = int(data_bin[32:64],2)
-                value_2 = struct.unpack('f', struct.pack('I', value_2))[0]
-                self.Controllers[ControllerID][ControllerIndex + 1] = value_2
-        else:
-            
-            value_1 = int(data_bin[0:32],2)
-            value_1 = struct.unpack('f', struct.pack('I', value_1))[0]
-            self.Controllers[ControllerID][ControllerIndex] = value_1
-
-
-
+        for key, val in zip(self.SensorKeys[msg_id], vals):
+            v = val / SENSOR_MULTIPLIER
+            self.Sensor_Raw[key] = v
+            self.Sensor_Val[key] = self.SensorLUT[key]['b'] + self.SensorLUT[key]['m'] * v
